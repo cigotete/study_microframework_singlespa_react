6d653a1bcc6d062a9f156eda1d7bb452ed85842a
Camilo Figueroa <cigotete@gmail.com>
2024-03-22 11:29:41 -0500

    Added dummy basic components.

---
+++ src/components/clientes.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Clientes = () => {
+  return (
+    <div>
+      <h1>Clientes -Component</h1>
+    </div>
+  );
+}
+
+export default Clientes;
---
+++ src/components/panel.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Panel = () => {
+  return (
+    <div className="panel-crm">
+      <h1>Panel -Component</h1>
+    </div>
+  );
+}
+
+export default Panel;
